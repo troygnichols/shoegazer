d5793eafd8e27f3b47e367bfa340d8deb358c16d
Troy Nichols <troygnichols@gmail.com>
2017-05-24 09:28:26 -0400

    Finish scraper

--- lib/shoegazer/scraper.ex
+++ lib/shoegazer/scraper.ex
@@ -33,14 +33,7 @@ defmodule Shoegazer.Scraper do
         # and get all tweets in between them,
         # saving them all as entries
         [last_tweet] = last_n_tweets(1)
-        cond do
-          last_tweet.id > most_recent_entry.twitter_id ->
-            # TODO: wrap transaction around this?
-            sync_tweets_with_entries(last_tweet, most_recent_entry)
-          true ->
-            Logger.debug "No new tweets, nothing to do"
-        end
-        :ok
+        sync_tweets_with_entries(last_tweet, most_recent_entry)
       :did_initialize ->
         # initialized a new set of local entries
         # with some tweets, we're done for now
@@ -48,24 +41,29 @@ defmodule Shoegazer.Scraper do
     end
   end
 
+  def entry_params_from_tweet(%{
+    "id" => id,
+    "entities"=> %{"urls" => urls},
+    "created_at" => posted_at
+  }) do
+    url = case urls do
+      [] -> nil
+      [%{"url" => url}|_] -> url
+    end
+    unix_now = DateTime.utc_now() |> DateTime.to_unix()
+    unix_posted_at = Timex.parse!(posted_at,
+      "%a %b %d %H:%M:%S %z %Y", :strftime)
+      |> DateTime.to_unix()
+    %{twitter_id: id, url: url, listened: false,
+      created_at: unix_now, posted_at: unix_posted_at}
+  end
+
   def maybe_init_local_entries do
     case Shoegazer.Entry.most_recent() do
       nil ->
         Logger.debug "Initializing local entries, fetching tweets"
         entries = last_n_tweets(50)
-        |> Enum.map(fn(%{"id_str" => id, "entities"=> %{"urls" => urls},
-          "created_at" => posted_at}) ->
-          url = case urls do
-            [] -> nil
-            [%{"url" => url}|_] -> url
-          end
-          unix_now = DateTime.utc_now() |> DateTime.to_unix()
-          unix_posted_at = Timex.parse!(posted_at,
-            "%a %b %d %H:%M:%S %z %Y", :strftime)
-            |> DateTime.to_unix()
-          %{twitter_id: id, url: url, listened: false,
-            created_at: unix_now, posted_at: unix_posted_at}
-        end)
+        |> Enum.map(&entry_params_from_tweet/1)
         Shoegazer.Repo.insert_all(Shoegazer.Entry, entries)
         :did_initialize
       entry ->
@@ -74,37 +72,42 @@ defmodule Shoegazer.Scraper do
     end
   end
 
-  def last_n_tweets(n, max_id \\ nil) do
+  def last_n_tweets(n, max_id \\ nil),
+    do: fetch_tweets(count: n, max_id: max_id)
+
+  def fetch_tweets(params) do
     url = "https://api.twitter.com/1.1/statuses/user_timeline.json"
-    params = [{"screen_name", "shoegazer_bot"}, {"count", n}]
-    params = case max_id do
-      nil -> params
-      max_id -> params ++ [{"max_id", max_id}]
-    end
     creds = OAuther.credentials(Application.get_env(:shoegazer, :twitter))
-    signed = OAuther.sign("get", url, params, creds)
+    signed = OAuther.sign("get", url, make_params(params), creds)
     {header, req_params} = OAuther.header(signed)
     %{status_code: 200, body: body} = HTTPoison.get!(url, [header], params: req_params)
     Poison.decode!(body)
   end
 
-  def sync_tweets_with_entries(%{id: twt_id},
-   %Shoegazer.Entry{twitter_id: entry_twt_id}) when entry_twt_id >= twt_id do
+  def make_params(params) do
+    params
+    |> Enum.reject(fn({_key, value}) -> is_nil(value) end)
+    |> Keyword.merge([screen_name: "shoegazer_bot"])
+    |> Enum.map(fn({key, value}) ->
+      {to_string(key), to_string(value)}
+    end)
+  end
+
+  # Starting from the newest entry twitter id from the database,
+  # get 20 tweets at a time until we reach our newest_tweet's id
+  def sync_tweets_with_entries(%{"id" => twt_id},
+   %{twitter_id: entry_twt_id}) when entry_twt_id >= twt_id do
     Logger.debug "Sync complete"
   end
-  def sync_tweets_with_entries(newest_tweet, newest_entry, chunk_size \\ 20) do
-    tweets = [newest_tweet] ++ last_n_tweets(chunk_size, newest_tweet.id)
-    last_tweet = List.last(tweets)
+  def sync_tweets_with_entries(newest_tweet, newest_entry) do
+    new_tweets = fetch_tweets(
+      since_id: newest_entry.twitter_id,
+      max_id: newest_tweet["id"], count: 20)
 
-    # starting from newest_entry twitter id (since_id param for twitter API)
-    # get 20 tweets at a time until we reach our newest_tweet's id
-    # then stop
+    entries = Enum.map(new_tweets, &entry_params_from_tweet/1)
+    Shoegazer.Repo.insert_all(Shoegazer.Entry, entries)
 
-    for tweet <- tweets do
-
-    end
-
-
-    sync_tweets_with_entries(last_tweet, newest_entry)
+    newest_entry_after_update = Enum.max_by(entries, &(&1.twitter_id))
+    sync_tweets_with_entries(newest_tweet, newest_entry_after_update)
   end
 end
